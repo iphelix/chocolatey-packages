fa3451bd1e9c5b4695ec1b891a3c3735d18f8b92
iphelix <iphelix@gmail.com>
2017-01-20 15:35:39 -0800

    Adding initial ollydbg2 commit.

---
+++ ollydbg2/tools/chocolateyinstall.ps1
@@ -0,0 +1,21 @@
+﻿$ErrorActionPreference = 'Stop'
+$packageName= 'OllyDbg2'
+
+$toolsDir   = ${Env:ProgramFiles(x86)}
+if(-Not ($toolsDir)) {
+  $toolsDir = ${Env:ProgramFiles}
+}
+$toolsDir = Join-Path $toolsDir $packageName
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  url           = 'http://www.ollydbg.de/odbg201.zip'
+  checksum      = '29244e551be31f347db00503c512058086f55b43c93c1ae93729b15ce6e087a5'
+  checksumType  = 'sha256'
+}
+Install-ChocolateyZipPackage @packageArgs
+
+$target = Join-Path $toolsDir "ollydbg.exe"
+$shortcut = "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\" + $packageName + ".lnk"
+Install-ChocolateyShortcut -shortcutFilePath $shortcut -targetPath $target
---
+++ ollydbg2/tools/chocolateyuninstall.ps1
@@ -0,0 +1,12 @@
+﻿$ErrorActionPreference = 'Stop';
+$packageName = 'OllyDbg2'
+
+$toolsDir = ${Env:ProgramFiles(x86)}
+if(-Not ($toolsDir)) {
+  $toolsDir = ${Env:ProgramFiles}
+}
+$toolsDir = Join-Path $toolsDir $packageName
+$shortcut = "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\" + $packageName + ".lnk"
+
+Remove-Item $toolsDir -Recurse
+Remove-Item $shortcut
