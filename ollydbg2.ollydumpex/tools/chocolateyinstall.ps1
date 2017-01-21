02aae49c4808269d58d8a99d65002e29b9aedc4b
iphelix <iphelix@gmail.com>
2017-01-20 17:46:19 -0800

    Adding OllyDumpEx plugin for OllyDbg2

---
+++ ollydbg2.ollydumpex/tools/chocolateyinstall.ps1
@@ -0,0 +1,29 @@
+﻿$ErrorActionPreference = 'Stop';
+$packageName = 'OllyDbg2'
+
+$toolsDir = ${Env:ProgramFiles(x86)}
+if(-Not ($toolsDir)) {
+  $toolsDir = ${Env:ProgramFiles}
+}
+$toolsDir = Join-Path $toolsDir 'OllyDbg2'
+
+$zipDir = Join-Path $toolsDir 'OllyDumpEx_v1.50'
+
+$packageArgs = @{
+  packageName   = $packageName
+  unzipLocation = $toolsDir
+  url           = 'https://low-priority.appspot.com/ollydumpex/OllyDumpEx.zip'
+  checksum      = 'a7fc263e1c5a096801f320c4d2a949a801928c4965c11f7913bed29cfd70d029'
+  checksumType  = 'sha256'
+}
+Install-ChocolateyZipPackage @packageArgs
+
+$pluginFile = 'OllyDumpEx_Od20.dll'
+$pluginZipPath = Join-Path $zipDir $pluginFile
+$pluginToolsPath = Join-Path $toolsDir $pluginFile
+
+# Move correct plugin into the tool directory
+Move-Item -Path $pluginZipPath -Destination $pluginToolsPath -Force -ErrorAction Stop
+
+# Delete temporary download directory
+Remove-Item -Path $zipDir -Recurse
---
+++ ollydbg2.ollydumpex/tools/chocolateyuninstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop';
+$packageName = 'OllyDbg2'
+
+$toolsDir = ${Env:ProgramFiles(x86)}
+if(-Not ($toolsDir)) {
+  $toolsDir = ${Env:ProgramFiles}
+}
+$toolsDir = Join-Path $toolsDir $packageName
+
+$pluginFile = 'OllyDumpEx_Od20.dll'
+$pluginToolsPath = Join-Path $toolsDir $pluginFile
+
+Remove-Item $pluginToolsPath
