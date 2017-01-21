2802b50db8b846ef04170db569eb204248661f07
iphelix <iphelix@gmail.com>
2017-01-20 17:38:22 -0800

    Adding OllyDumpEx for Ollydbg plugin

---
+++ ollydbg.ollydumpex/tools/chocolateyuninstall.ps1
@@ -0,0 +1,13 @@
+﻿$ErrorActionPreference = 'Stop';
+$packageName = 'OllyDbg'
+
+$toolsDir = ${Env:ProgramFiles(x86)}
+if(-Not ($toolsDir)) {
+  $toolsDir = ${Env:ProgramFiles}
+}
+$toolsDir = Join-Path $toolsDir $packageName
+
+$pluginFile = 'OllyDumpEx_Od11.dll'
+$pluginToolsPath = Join-Path $toolsDir $pluginFile
+
+Remove-Item $pluginToolsPath
